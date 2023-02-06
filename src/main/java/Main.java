a1231afd37d97ba3d77ea444b91d451fd3f9015c
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2023-02-06 21:10:24 +0700

    refactor: change bot name, add new package to implement greedy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -58,7 +58,7 @@ public class Main {
 
         Thread.sleep(1000);
         System.out.println("Registering with the runner...");
-        hubConnection.send("Register", token, "agario supremacy");
+        hubConnection.send("Register", token, "agario>>>");
 
         //This is a blocking call
         hubConnection.start().subscribe(() -> {
